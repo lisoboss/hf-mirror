7cf5dffc4e3ba2f9b86f50c069256da43726471e
lisoboss <37949544+lisoboss@users.noreply.github.com>
2025-02-21 18:40:02 +0800

    fix(api): update hf_repo_info api

--- src/agent/api.rs
+++ src/agent/api.rs
@@ -1,4 +1,8 @@
-use crate::{error::AppError, git::file_tree, HF_MIRROR_PATH};
+use crate::{
+    error::AppError,
+    git::{commit_head_id, file_tree},
+    HF_MIRROR_PATH,
+};
 use axum::{extract::Path, Json};
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
@@ -53,12 +57,32 @@ pub(crate) async fn hf_list_repo_files(
 }
 
 #[derive(Debug, Serialize)]
+pub(crate) struct Sibling {
+    rfilename: String,
+}
+
+#[derive(Debug, Serialize)]
 pub(crate) struct RepoInfo {
     id: String,
+    sha: String,
+    siblings: Vec<Sibling>,
 }
 
 pub(crate) async fn hf_repo_info(Path(hf_api): Path<HFApi>) -> Result<Json<RepoInfo>, AppError> {
+    let repo_path = PathBuf::from(&*HF_MIRROR_PATH)
+        .join(&hf_api.resource_name)
+        .join(&hf_api.user_id)
+        .join(&hf_api.repo_id);
+
+    let sha = commit_head_id(&repo_path)?;
+    let mut siblings: Vec<Sibling> = Vec::new();
+    file_tree(&repo_path, |path, _, _| {
+        siblings.push(Sibling { rfilename: path });
+    })?;
+
     Ok(Json(RepoInfo {
         id: format!("{}/{}", hf_api.user_id, hf_api.repo_id),
+        sha,
+        siblings,
     }))
 }
--- src/main.rs
+++ src/main.rs
@@ -22,24 +22,28 @@ const HF_MIRROR_ADDR: LazyCell<String> = LazyCell::new(|| {
 async fn main() {
     let app = Router::<()>::new()
         // https://huggingface.co/api/models/meta-llama/Llama-3.3-70B-Instruct/tree/main?recursive=True&expand=False
-        // https://huggingface.co/api/models/casperhansen/deepseek-r1-distill-llama-8b-awq/tree/main?recursive=True&expand=False
         .route(
-            "/api/{resource_name}/{user_id}/{repo_id}/tree/main",
+            "/api/{resource_name}/{user_id}/{repo_id}/tree/{revision}",
             get(agent::hf_list_repo_files),
         )
+        // https://huggingface.co/api/models/casperhansen/deepseek-r1-distill-llama-8b-awq/revision/main
+        .route(
+            "/api/{resource_name}/{user_id}/{repo_id}/revision/{revision}",
+            get(agent::hf_repo_info),
+        )
+        // https://huggingface.co/api/models/casperhansen/deepseek-r1-distill-llama-8b-awq/revision/main
         .route(
             "/api/{resource_name}/{user_id}/{repo_id}",
             get(agent::hf_repo_info),
         )
         // https://huggingface.co/datasets/deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B/resolve/main/generation_config.json
         .route(
-            "/{resource_name}/{user_id}/{repo_id}/resolve/main/{*file_path}",
+            "/{resource_name}/{user_id}/{repo_id}/resolve/{revision}/{*file_path}",
             get(agent::hf_hub_download),
         )
-        // https://huggingface.co/meta-llama/Llama-3.3-70B-Instruct/tree/main/special_tokens_map.json
         // https://huggingface.co/deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B/resolve/main/generation_config.json
         .route(
-            "/{user_id}/{repo_id}/resolve/main/{*file_path}",
+            "/{user_id}/{repo_id}/resolve/{revision}/{*file_path}",
             get(agent::hf_hub_download),
         );
 
